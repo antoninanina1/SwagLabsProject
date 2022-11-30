9579da7d1d10089ae957b9f2aa054b6b721747e1
QA36 <QA36@DESKTOP-QPQFVHR>
2022-11-30 21:11:55 +0100

    Init

---
+++ SwagProject/SwagProject/Page/LoginPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using SwagProject.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwagProject.Page
+{
+    public class LoginPage
+    {
+        private IWebDriver driver = WebDrivers.Instance;
+        public IWebElement Username => driver.FindElement(By.Id("user-name"));
+        public IWebElement Password => driver.FindElement(By.Id("password"));
+        public IWebElement LoginButton => driver.FindElement(By.Id("login-button"));
+
+        public void LoginOnPage(string name, string pass)
+        {
+            Username.SendKeys(name);
+            Password.SendKeys(pass);
+            LoginButton.Submit();
+        }
+    }
+}
---
+++ SwagProject/SwagProject/Tests/ByProductTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using SwagProject.Driver;
+using SwagProject.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwagProject.Tests
+{
+    public class ByProductTest
+    {
+        private LoginPage loginPage;
+        private ProductPage productPage;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            WebDrivers.Initialize();
+            loginPage = new LoginPage();
+            productPage = new ProductPage();
+
+        }
+        
+        [TearDown]
+        public void CleanUp()
+        {
+            WebDrivers.CleanUp();
+        }
+
+        [Test] 
+        public void TC01_AddToProductInCard_ShouldDisplayedTwoProduct()
+        {
+            loginPage.Username.SendKeys("standard_user");
+            loginPage.Password.SendKeys("secret_sauce");
+            loginPage.LoginButton.Submit();
+
+            productPage.AddBackPack.Click();
+            productPage.AddT_Shirt.Click();
+
+            Assert.That("2",Is.EqualTo(productPage.Cart.Text));
+        }
+
+    
+
+       
+    }
+     
+}
