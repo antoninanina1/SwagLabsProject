81ce42a3019f9d0d26f33f7306c2b7bffc6d935f
QA36 <QA36@DESKTOP-QPQFVHR>
2022-12-01 19:44:38 +0100

    NewProductTest

--- SwagProject/SwagProject/Driver/WebDrivers.cs
+++ SwagProject/SwagProject/Driver/WebDrivers.cs
@@ -5,22 +5,25 @@ namespace SwagProject.Driver
 {
     public class WebDrivers
     {
-            public static IWebDriver? Instance { get; set; }
+        public static IWebDriver? Instance { get; set; }
 
-            public static void Initialize()
-            {
-                Instance = new ChromeDriver();
-                Instance.Manage().Window.Maximize();
-                Instance.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
-                Instance.Navigate().GoToUrl("https://www.saucedemo.com/");
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver();
+            Instance.Manage().Window.Maximize();
+            Instance.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(50);
+            Instance.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+            Instance.Navigate().GoToUrl("https://www.saucedemo.com/");
 
-            }
+        }
 
-            public static void CleanUp()
-            {
-                Instance?.Quit();
-            }
-        
+
+
+        public static void CleanUp()
+        {
+            Instance?.Quit();
+        }
+
 
     }
 }
